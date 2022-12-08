355d14556ed17519dbfbe37e215ea89eb1793477
Nahuel <nahuel-sanchez@outlook.com>
2022-12-08 12:54:34 -0300

    trabajo arreglado

--- blog/apps/usuarios/forms.py
+++ blog/apps/usuarios/forms.py
@@ -5,21 +5,24 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
 
 class RegistroForm(UserCreationForm):
-    email = forms.EmailField(label='Correo', required=True)
-    first_name = forms.CharField(label='Nombre', required=True)
-    last_name = forms.CharField(label='Apellido', required=True)
-    password1 = forms.CharField(
-        label='Contraseña', widget=forms.PasswordInput, required=True)
-    password2 = forms.CharField(
-        label='Confirmar Contraseña', widget=forms.PasswordInput, required=True)
+	email = forms.EmailField(label='Correo', required=True)
+	first_name = forms.CharField(label='Nombre', required=True)
+	last_name = forms.CharField(label='Apellido', required=True)
+	password1 = forms.CharField(
+		label='Contraseña', widget=forms.PasswordInput, required=True)
+	password2 = forms.CharField(
+		label='Confirmar Contraseña', widget=forms.PasswordInput, required=True)
 
-    class Meta:
-        model = Usuario
-        fields = [
-            'first_name',
-            'last_name',
-            'username',
-            'email',
-            'password1',
-            'password2'
-        ]
+	class Meta:
+		model = Usuario
+		fields = [
+			'first_name',
+			'last_name',
+			'username',
+			'email',
+			'password1',
+			'password2'
+		]
+	def __init__(self,*args,**kwargs):
+		super(RegistroForm, self).__init__(*args, **kwargs)
+		self.fields["username"].help_text = None
