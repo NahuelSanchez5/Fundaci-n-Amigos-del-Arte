83c56c577d78dc58743efa24d5778cc164081daf
Nahuel <nahuel-sanchez@outlook.com>
2022-12-13 11:25:06 -0300

    Actualizacion clase vista noticias

--- blog/apps/noticias/views.py
+++ blog/apps/noticias/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Noticia
 
+@login_required
 def Listar_Noticias(request):
-    return render(request, 'noticias/listar.html')
+    contexto = {}
+
+    n = Noticia.objects.all()
+
+    contexto['noticias'] = n
+    
+    return render(request, 'noticias/listar.html', contexto)
+
+@login_required    
+def Detalle_Noticias(request,pk):
+    contexto = {}
+    n = Noticia.objects.get(pk = pk) #RETORNA SOLO UN OBJETO
+    contexto['noticia'] = n
+
+    return render(request, "noticias/detalle.html", contexto)
+
+
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+
 from django.urls import path, include
+
 from . import views
+
 from django.contrib.auth import views as auth
+
+from django.conf import settings
+
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -35,4 +43,5 @@ urlpatterns = [
     path('Usuario/',include('apps.usuarios.urls')),
     
     path('Contacto/', views.Contacto, name = 'contacto'),
-]
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
