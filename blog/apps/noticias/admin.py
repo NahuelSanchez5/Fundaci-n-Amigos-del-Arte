0ce77f910452c50f3f405b9df1acead5901d9f7c
Nahuel <nahuel-sanchez@outlook.com>
2022-12-16 16:32:22 -0300

    Eliminar comentarios. Vane, Dalmiro, Aaron, Rosana, Joaquin, Nahuel

--- blog/apps/noticias/admin.py
+++ blog/apps/noticias/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
+
 # Register your models here.
 
-from .models import Categoria, Noticia
+from .models import Categoria, Noticia, Comentario
 admin.site.register(Categoria)
 admin.site.register(Noticia)
+admin.site.register(Comentario)
